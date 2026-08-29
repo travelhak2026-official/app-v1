2a721f8d12d9f0cbfaeeb7879ea2e8f10ae45f7a
Dyad <travelhak2026@gmail.com>
2026-08-29 15:33:48 +0530

    Fix PostCSS Config Module Error

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,5 @@
+import typography from "@tailwindcss/typography";
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
@@ -13,6 +15,6 @@ export default {
     },
   },
   plugins: [
-    require("@tailwindcss/typography"),
+    typography,
   ],
-}
+};
